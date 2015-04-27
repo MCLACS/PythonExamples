b00abaa2961f09fcabd2be3ad8f5a4b6875d5efb
MarkACohen <mark@cassconsulting.biz>
2015-04-27 09:23:46 -0400

    changes to module example

--- ModuleDriver.py
+++ ModuleDriver.py
@@ -3,3 +3,8 @@ import Module
 print Module.evens([1, 2, 3, 4, 5, 6])
 print Module.evens([11, 12, 13, 14, 15, 16])
 print Module.count
+
+reload(Module)
+
+print Module.evens([11, 12, 13, 14, 15, 16])
+print Module.count
